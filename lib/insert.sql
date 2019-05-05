6670b7629263a4a695fdd419a5ea1a6aa6066f62
Sara Harvey <saraharveynyc@gmail.com>
2019-05-05 17:26:06 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,5 +3,6 @@ INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Rowdy',
 INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Tabitha', 3,'F','brown','Scottish Fold', 'Y');
 INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Sergeant Brown', 3,'M','brown','Scottish Fold', 'Y');
 INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Melissa', 3,'F','brown','Scottish Fold', 'Y');
+INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Grinch', 3,'F','brown','Scottish Fold', 'Y');
 INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES ('Wendy', 3,'F','brown','Scottish Fold', 'Y');
 INSERT INTO cats (name, age, gender, color, temperament, alive) VALUES (null, 3,'F','brown', 'Scottish Fold', 'Y');
